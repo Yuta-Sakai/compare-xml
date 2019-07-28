386ee3c9ced2c4589c6e7d7f2e826057d1ca0017
Yuta Sakai <yuta.sakai0313@gmail.com>
2019-07-28 21:54:44 +0900

    stduy how to use showQuickPick

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { createSocket } from 'dgram';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,11 +14,29 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.comparexml', () => {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		//var compared = vscode.window.activeTextEditor;
+		//
+		//if(!compared)
+		//{
+		//	return;
+		//}
+
+		//var answer;
+		//var options: vscode.InputBoxOptions = {
+		//	prompt: "Label",
+		//	placeHolder: "(placeholder)",
+		//	valueSelection: [0,1]
+		//};
+		//
+		//vscode.window.showInputBox(options).then(value => {
+		//	if(!value){return;}
+		//	answer = value;
+		//});
+		
+		vscode.window.showQuickPick(["a", "i"]);
 	});
 
 	context.subscriptions.push(disposable);
